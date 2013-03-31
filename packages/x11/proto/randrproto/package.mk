58838b82eb97089415b82b8466ac9d2beea18eb4
Stephan Raue <stephan@openelec.tv>
2013-03-31 10:46:04 +0200

    randrproto: convert to new package scheme
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/proto/randrproto/package.mk
+++ packages/x11/proto/randrproto/package.mk
@@ -31,6 +31,9 @@ PKG_PRIORITY="optional"
 PKG_SECTION="x11/proto"
 PKG_SHORTDESC="randrproto: Randr extension headers"
 PKG_LONGDESC="Randr extension headers"
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+# package specific configure options
+PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
