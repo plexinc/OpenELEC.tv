dabb625852135e2d892cad431bd09b55099b7214
Stephan Raue <stephan@openelec.tv>
2013-03-31 10:46:04 +0200

    scrnsaverproto: convert to new package scheme
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/proto/scrnsaverproto/package.mk
+++ packages/x11/proto/scrnsaverproto/package.mk
@@ -31,6 +31,9 @@ PKG_PRIORITY="optional"
 PKG_SECTION="x11/proto"
 PKG_SHORTDESC="scrnsaverproto: ScrnSaver extension headers"
 PKG_LONGDESC="ScrnSaver extension headers"
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+# package specific configure options
+PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
