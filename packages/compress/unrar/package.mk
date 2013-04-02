fa0f1abd74925eafdc7e7036e536051c12fbc0f4
Stephan Raue <stephan@openelec.tv>
2013-04-02 12:45:35 +0200

    unrar: convert to new package scheme
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/compress/unrar/package.mk
+++ packages/compress/unrar/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,13 +18,33 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="unrar"
+PKG_VERSION="4.2.4"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="free"
+PKG_SITE="http://www.rarlab.com"
+PKG_URL="http://www.rarlab.com/rar/unrarsrc-$PKG_VERSION.tar.gz"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="compress"
+PKG_SHORTDESC="unrar: Extract, test and view RAR archives"
+PKG_LONGDESC="Unrar is a package to handle files compressed in the RAR format. Due to strange licensing issues this package can only view, test and extract files in a given archive, but not pack files. But since we have far more advanced open-source compression utils it should be enough to extract the content when you get a RAR archive."
 
-cd $BUILD/$1
-make CXX="$TARGET_CXX" \
-     CXXFLAGS="$TARGET_CXXFLAGS" \
-     RANLIB="$TARGET_RANLIB" \
-     AR="$TARGET_AR" \
-     STRIP="$TARGET_STRIP" \
-     -f makefile.unix
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
 
+make_target() {
+  make CXX="$TARGET_CXX" \
+       CXXFLAGS="$TARGET_CXXFLAGS" \
+       RANLIB="$TARGET_RANLIB" \
+       AR="$TARGET_AR" \
+       STRIP="$TARGET_STRIP" \
+       -f makefile.unix
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+    cp unrar $INSTALL/usr/bin
+}
