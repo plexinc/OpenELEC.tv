804f3e0c33d353255055ee08980d57d0523011d1
Stephan Raue <stephan@openelec.tv>
2013-04-04 16:40:37 +0200

    bison: convert to new package scheme
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/toolchain/devel/bison/package.mk
+++ packages/toolchain/devel/bison/package.mk
@@ -26,11 +26,21 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/bison/"
 PKG_URL="http://ftp.gnu.org/gnu/bison/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="ccache"
+PKG_BUILD_DEPENDS_HOST="ccache"
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="bison: The GNU general-purpose parser generator"
 PKG_LONGDESC="Bison is a general-purpose parser generator that converts a grammar description for an LALR(1) context-free grammar into a C program to parse that grammar. Once you are proficient with Bison, you may use it to develop a wide range of language parsers, from those used in simple desk calculators to complex programming languages. Bison is upward compatible with Yacc: all properly-written Yacc grammars ought to work with Bison with no change. Anyone familiar with Yacc should be able to use Bison with little trouble. You need to be fluent in C programming in order to use Bison or to understand this manual."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+# package specific configure options
+PKG_CONFIGURE_OPTS_HOST="--disable-rpath --with-gnu-ld"
+
+post_configure_host() {
+ # The configure system causes Bison to be built without support for
+ # internationalization of error messages if a bison program is not already in
+ # $PATH. The following addition will correct this:
+   echo '#define YYENABLE_NLS 1' >> lib/config.h
+}
