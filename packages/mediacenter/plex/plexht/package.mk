a009bab2999310e2faae5ade692e8720256f09e3
Mark Walker <mw@tdchosting.dk>
2014-02-21 01:56:28 +0100

    Don't fetch src when building HEAD

--- packages/mediacenter/plex/plexht/package.mk
+++ packages/mediacenter/plex/plexht/package.mk
@@ -28,7 +28,9 @@ PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.plexapp.com"
-PKG_URL="https://github.com/plexinc/plex-home-theater-public/archive/pht-frodo.zip"
+if [ "$PHT_HEAD" != "HEAD" ]; then
+  PKG_URL="https://github.com/plexinc/plex-home-theater-public/archive/pht-frodo.zip"
+fi
 PKG_DEPENDS="ffmpeg Python zlib bzip2 systemd libass curl rtmpdump fontconfig freetype libmad libogg libmodplug faad2 flac libmpeg2 taglib service.openelec.settings SDL_mixer SDL_image"
 PKG_BUILD_DEPENDS_TARGET="ffmpeg toolchain boost Python zlib bzip2 systemd lzo pcre swig libass enca curl libmicrohttpd libssh rtmpdump fontconfig fribidi tinyxml libjpeg-turbo libpng tiff freetype jasper libmad libsamplerate libogg libcdio libmodplug faad2 fribidi flac flac++ libmpeg2 taglib yajl sqlite libshairport SDL_image SDL_mixer avahi glew libplist libcec lame"
 PKG_PRIORITY="optional"
