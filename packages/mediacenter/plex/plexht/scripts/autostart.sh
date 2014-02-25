bb282e87985c278d3da6b1ff8edc8b138c149940
Mark Walker <mw@tdchosting.dk>
2014-02-25 15:24:33 +0100

    Fixing NUC IR module reload

---
+++ packages/mediacenter/plex/plexht/scripts/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ "`lsmod|grep nuvoton-cir|wc -l`" -gt "0" ]; then
+modprobe -r nuvoton-cir
+echo "auto" > "/sys/bus/acpi/devices/NTN0530:00/physical_node/resources"
+modprobe nuvoton-cir
+fi
