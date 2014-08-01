447496299c9c94c08188bcb2f8ac7c5b0fb5b7ff
Mark Walker <ziggimon@raindogs.dk>
2014-08-01 20:24:48 +0200

    Fixing i5 IR upstrart on NUC

--- packages/mediacenter/plex/plexht/scripts/autostart.sh
+++ packages/mediacenter/plex/plexht/scripts/autostart.sh
@@ -3,6 +3,7 @@
 if [ "`lsmod|grep nuvoton-cir|wc -l`" -gt "0" ]; then
 modprobe -r nuvoton_cir
 echo "auto" > "/sys/bus/acpi/devices/NTN0530\:00/physical_node/resources"
+echo "auto" > "/sys/bus/acpi/devices/NTN0530\:00/physical_node/resources"
 modprobe nuvoton_cir
 fi
 
