d4e408938bdd54fb6cdeadd8f2e849d491ea7ca1
LongChair <LongChair@hotmail.com>
2015-03-04 02:21:25 -0800

    Add libevdev to qt dependencies
    That should help getting proper keyboard usage with qt.

--- packages/multimedia/qt/package.mk
+++ packages/multimedia/qt/package.mk
@@ -30,7 +30,7 @@ case $PROJECT in
 		PKG_BUILD_DEPENDS_TARGET="bzip2 Python zlib:host zlib libpng tiff dbus glib fontconfig openssl linux-headers glibc alsa libxcb libXcursor libXtst pciutils pulseaudio nss"
 	;;
 	RPi|RPi2)
-		PKG_DEPENDS_TARGET="curl bcm2835-driver bzip2 Python zlib:host zlib libpng tiff dbus glib fontconfig glibc liberation-fonts-ttf font-util font-xfree86-type1 font-misc-misc alsa flex bison ruby icu sqlite libX11 xrandr libXdmcp libxslt libXcomposite libwebp"
+		PKG_DEPENDS_TARGET="curl bcm2835-driver bzip2 Python zlib:host zlib libpng tiff dbus glib fontconfig glibc liberation-fonts-ttf font-util font-xfree86-type1 font-misc-misc alsa flex bison ruby icu sqlite libX11 xrandr libXdmcp libxslt libXcomposite libwebp libevdev"
 		PKG_BUILD_DEPENDS_TARGET="bcm2835-driver bzip2 Python zlib:host zlib libpng tiff dbus glib fontconfig mysql openssl linux-headers glibc alsa"
 
 	;;
@@ -72,7 +72,7 @@ case $PROJECT in
                                                         -prefix /usr/local/qt5 \
                                                         -hostprefix ${ROOT}/${BUILD} \
                                                         -v \
-                                                        -debug \
+                                                        -release \
                                                         -opensource \
                                                         -confirm-license \
                                                         -optimized-qmake \
