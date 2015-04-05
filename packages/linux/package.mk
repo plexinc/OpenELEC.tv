ad7b34338e3b9afec3df0142db004ea374623a4a
LongChair <LongChair@hotmail.com>
2015-04-05 04:45:00 -0700

    Make default linux configuration use 3.19.3

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -30,6 +30,10 @@ case "$LINUX" in
     PKG_VERSION="3.19.3"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
+  *)
+    PKG_VERSION="3.19.3"
+    PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
+    ;;
 esac
 PKG_REV="1"
 PKG_ARCH="any"
