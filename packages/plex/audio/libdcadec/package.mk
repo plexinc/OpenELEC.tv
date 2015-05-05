d707c2226442a8350eda66190516cf9118df09af
LongChair <LongChair@hotmail.com>
2015-05-05 11:07:57 -0700

    Add libdcadec package

---
+++ packages/plex/audio/libdcadec/package.mk
@@ -0,0 +1,40 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libdcadec"
+PKG_VERSION="0.1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://nightlies.plex.tv"
+PKG_URL="$PKG_SITE/plex-oe-sources/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="audio"
+PKG_SHORTDESC="libdcadec : DTS Coherent Acoustics decoder with support for HD extensions"
+PKG_LONGDESC="libdcadec : DTS Coherent Acoustics decoder with support for HD extensions"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+makeinstall_target() {
+
+  cd ${ROOT}/${BUILD}/${PKG_NAME}-${PKG_VERSION}
+  make install DESTDIR=${SYSROOT_PREFIX}
+  mv ${SYSROOT_PREFIX}/dcadec.pc ${SYSROOT_PREFIX}/usr/lib/pkgconfig
+}
