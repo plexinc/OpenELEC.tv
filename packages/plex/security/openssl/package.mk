587b55d95b616b802eae13a15f80bedb8553b3c9
Mark Walker <mark@plex.tv>
2015-06-25 10:10:24 +0200

    Fix SSL missing from cmake and bump openssl to latest

--- packages/plex/security/openssl/package.mk
+++ packages/plex/security/openssl/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="openssl"
-PKG_VERSION="1.0.2a"
+PKG_VERSION="1.0.2c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
