1152c50245f070e6354e9bafc632cf116285ff71
Lionel CHAZALLON <longchair@hotmail.com>
2015-09-24 21:11:34 +0200

    Add libXScrnSaver package
    this is a new qt 5.6 dependency on x11

---
+++ packages/plex/x11/lib/libXScrnSaver/package.mk
@@ -0,0 +1,33 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libXScrnSaver"
+PKG_VERSION="1.2.2"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE=" http://ftp.x.org/pub/individual/lib/"
+PKG_URL=" http://ftp.x.org/pub/individual/lib/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/lib"
+PKG_SHORTDESC="libXScrnSaver The XScrnSaver library"
+PKG_LONGDESC="libXScrnSaver The XScrnSaver library"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
