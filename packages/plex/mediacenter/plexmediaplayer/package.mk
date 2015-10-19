f62b859fbc410bd4507cd425779a0f929a94c888
Mark Walker <ziggimon@raindogs.dk>
2015-10-19 13:09:33 +0200

    Adding $BUILD variable :/

--- packages/plex/mediacenter/plexmediaplayer/package.mk
+++ packages/plex/mediacenter/plexmediaplayer/package.mk
@@ -67,7 +67,7 @@ unpack() {
           git clone --depth 20 -b $PKG_VERSION git@github.com:plexinc/$PMP_REPO.git $BUILD/${PKG_NAME}-${PKG_VERSION}
           if [ ! -z "$CI_BUILD" ]; then
             if [ "`git --git-dir=$BUILD/${PKG_NAME}-${PKG_VERSION}/.git --work-tree=$BUILD/${PKG_NAME}-${PKG_VERSION} log --pretty=%H|grep -c $PMP_RELEASE_SHA`" = "1" ]; then
-              git --git-dir=plexmediaplayer-dist-ninja/.git --work-tree=plexmediaplayer-dist-ninja checkout $PMP_RELEASE_SHA
+              git --git-dir=$BUILD/plexmediaplayer-dist-ninja/.git --work-tree=$BUILD/plexmediaplayer-dist-ninja checkout $PMP_RELEASE_SHA
             else
               echo "There are more than 20 commits in the REPO since the release build was initiated. Erroring out!"
               exit 1
