8818eb2b75644712b3faf59e96a30b27cea3e784
Mark Walker <mark@plexapp.com>
2015-10-26 10:21:02 +0100

    Add CI branch awareness when building from different upstream branches.

--- packages/plex/mediacenter/plexmediaplayer/package.mk
+++ packages/plex/mediacenter/plexmediaplayer/package.mk
@@ -21,10 +21,10 @@ PKG_NAME="${MEDIACENTER,,}"
 
 case $PROJECT in
      Generic|Nvidia_Legacy)
-     PKG_VERSION="dist-ninja"
+     PKG_VERSION="${PMP_BRANCH:-dist-ninja}"
      ;;
      RPi|RPi2)
-     PKG_VERSION="dist-ninja"
+     PKG_VERSION="${PMP_BRANCH:-dist-ninja}"
      ;;
 esac
 
@@ -69,7 +69,7 @@ set -x
           git clone --depth 20 -b $PKG_VERSION git@github.com:plexinc/$PMP_REPO.git $BUILD/${PKG_NAME}-${PKG_VERSION}
           if [ ! -z "$PMP_RELEASE_SHA" ]; then
             if [ "`git --git-dir=$BUILD/${PKG_NAME}-${PKG_VERSION}/.git --work-tree=$BUILD/${PKG_NAME}-${PKG_VERSION} log --pretty=%H|grep -c $PMP_RELEASE_SHA`" = "1" ]; then
-              git --git-dir=$BUILD/plexmediaplayer-dist-ninja/.git --work-tree=$BUILD/plexmediaplayer-dist-ninja checkout $PMP_RELEASE_SHA
+              git --git-dir=$BUILD/plexmediaplayer-${PKG_VERSION}/.git --work-tree=$BUILD/plexmediaplayer-${PKG_VERSION} checkout $PMP_RELEASE_SHA
             else
               echo "There are more than 20 commits in the REPO since the release build was initiated. Erroring out!"
               exit 1
