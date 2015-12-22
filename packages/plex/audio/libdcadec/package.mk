3c29b303c88b163d1c0f109296ad2a114fca1a37
Mark Walker <ziggimon@raindogs.dk>
2015-12-22 23:47:40 +0100

    Bump libdcadec to newer version.

--- packages/plex/audio/libdcadec/package.mk
+++ packages/plex/audio/libdcadec/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libdcadec"
-PKG_VERSION="0.1"
+PKG_VERSION="0.1.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
